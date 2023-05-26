ad85f8c3b4de1d621f5db0b2509cb560cb0a3da5
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-05-26 22:55:29 +0300

    finish task 8.2 SimpleLightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+const imgGalleryEl = document.querySelector('.gallery');
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      (item) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"src="${item.preview}"
+        alt="${item.description}"
+        title="${item.description}"
+        />
+      </a>
+    </li>`
+    )
+    .join('');
+}
+
+const addGalleryMarkup = createGalleryItems(galleryItems);
+imgGalleryEl.innerHTML = addGalleryMarkup;
+imgGalleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+new SimpleLightbox('.gallery a', {captionDelay:250});
+  
+  imgGalleryEl.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
